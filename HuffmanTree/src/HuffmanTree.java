c7ad34293358a8225ad2fbf2dd85be6d836d7ed0
Jacob Harrison <jgharrison@uiowa.edu>
2021-10-18 15:47:00 -0500

    Second commit

--- HuffmanTree/src/HuffmanTree.java
+++ HuffmanTree/src/HuffmanTree.java
@@ -1,3 +1,46 @@
+import java.util.*;
+
 public class HuffmanTree {
-    int test = 0;
+    HashMap<String,Integer> SymbolToFrequency = new HashMap<String, Integer>();
+
+    HuffmanTree(){
+        SymbolToFrequency.put("a",2);
+        SymbolToFrequency.put("b",5);
+        SymbolToFrequency.put("c",3);
+        SymbolToFrequency.put("d",1);
+        SymbolToFrequency.put("e",7);
+        SymbolToFrequency.put("f",9);
+
+        while(!SymbolToFrequency.keySet().isEmpty()){
+            Integer smallest = 1000000;
+            Integer secondSmallest = 1000000;
+            for(String symbol : SymbolToFrequency.keySet()){
+                Integer frequency = SymbolToFrequency.get(symbol);
+                if(frequency < smallest){
+                    secondSmallest = smallest;
+                    smallest = frequency;
+                }
+                else if(frequency < secondSmallest){
+                    secondSmallest = frequency;
+                }
+            }
+
+        }
+
+
+    }
+
+
+    public class Node {
+        Node parent;
+        Node left;
+        Node right;
+        String m_symbol;
+        int m_frequency;
+
+        Node(String symbol, int frequency){
+            m_symbol = symbol;
+            m_frequency = frequency;
+        }
+    }
 }
